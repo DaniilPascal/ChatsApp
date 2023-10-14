1265b4d62e27484e65e089fe7b1ce9c94b8cfe66
DaniilPascal <105735993+DaniilPascal@users.noreply.github.com>
2023-10-14 17:49:41 +0300

    Add Phone Register

--- app/src/main/java/com/example/chatsapp/Activities/PhoneNumberActivity.java
+++ app/src/main/java/com/example/chatsapp/Activities/PhoneNumberActivity.java
@@ -2,15 +2,46 @@ package com.example.chatsapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chatsapp.R;
+import com.example.chatsapp.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null) {
+            Intent intent = new Intent(PhoneNumberActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+//        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OPTActivity.class);
+                intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 }
